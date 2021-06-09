00b846e5709d4b700082489f02b1c4542d4a03ca
Tippy0Typy <85592980+Zhadow0Floors@users.noreply.github.com>
2021-06-09 18:45:16 +0000

    Updated hello.js to print 'hello world!'

--- hello.js
+++ hello.js
@@ -1,7 +1,7 @@
 function hello() {
 	// TODO: change this string so that your program prints "Hello world!" 
 	// when you hit the replit run button
-	return ("Hello World!");
+	console.log("Hello world!");
 }
  
 module.exports = hello;
